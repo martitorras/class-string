3ea3b7512a12aaef3c82a2ca05bc0a671bbb6919
Martí Torras Isanta <martiti@TR12.CAT>
2018-10-03 13:57:19 +0200

    class

--- String/Header.h
+++ String/Header.h
@@ -1,27 +1,45 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
+#include <assert.h>
 
 typedef unsigned int uint;
 
 class string {
-public:
-	char* word;
-	uint length = 0;
+private:
+	char* word = nullptr;
+	uint length = 0u;
 
+public:
 	string() {};
-	string(const char* _word) {
-		while (_word[length] != '\0') ++length;
+	string(const char* my_word) {
+		assert(my_word != nullptr);
+		while (my_word[length] != '\0') ++length;
 
 		word = new char[length];
 
-		for (uint i = 0; i < length; ++i) word[i] = _word[i];
+		for (uint i = 0; i < length; ++i) word[i] = my_word[i];
+		//while(*word++ = *my_word++);
 	}
 
-	~string() {};
+	/*string(const string& my_word) {
+		while (my_word.length != '\0') ++length;
+
+		word = new char[length];
+
+		for (uint i = 0; i < length; ++i) word[i] = my_word.word[i];
+	}*/
+
+	/*~string() {
+		if (word != nullptr) {
+			delete[] word;
+			word = nullptr;
+			length = 0u;
+		}
+	}*/
 
 	string operator= (const char* my_word) {
 		while (my_word[length] != '\0') ++length;
-
+		delete[] word;
 		word = new char[length];
 
 		for (uint i = 0; i < length; ++i) word[i] = my_word[i];
